4dbc478e37244e0d2aca32608266fecb6634c91b
yzw <739265427@qq.com>
2019-11-27 15:12:03 +0800

    初始化项目

---
+++ src/main/java/com/yzw/twelve/TwelveApplication.java
@@ -0,0 +1,13 @@
+package com.yzw.twelve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwelveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TwelveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yzw/twelve/TwelveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yzw.twelve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwelveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
